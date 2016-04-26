0eb0aa3e130b4d2f89fd0b3e50a334574c122fbf
Victor M <victoremepunto@gmail.com>
2016-04-26 23:47:19 +0200

    Added Connection strings and User Model

---
+++ Users/Models/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Users.Models
+{
+    public class AppUser : IdentityUser
+    {
+        // Additional properties to be added here ...
+    }
+}
