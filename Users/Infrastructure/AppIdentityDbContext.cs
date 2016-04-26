b2444ea349f8dee4333745b98542d095db13429c
Victor M <victoremepunto@gmail.com>
2016-04-27 00:02:37 +0200

    finished CRUD

---
+++ Users/Infrastructure/AppIdentityDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Users.Models;
+using System.Data.Entity;
+
+namespace Users.Infrastructure
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext() : base("IdentityDb") { }
+
+        static AppIdentityDbContext()
+        {
+            Database.SetInitializer<AppIdentityDbContext>(new IdentityDbInit());
+        }
+        public static AppIdentityDbContext Create()
+        {
+            return new AppIdentityDbContext();
+        }
+
+        public class IdentityDbInit: DropCreateDatabaseIfModelChanges<AppIdentityDbContext>
+        {
+            protected override void Seed(AppIdentityDbContext context)
+            {
+                PerformInitialSetup(context);
+                base.Seed(context);
+            }
+            public void PerformInitialSetup(AppIdentityDbContext context)
+            {
+                // initial configuration will go here
+            }
+        }
+    }
+}
